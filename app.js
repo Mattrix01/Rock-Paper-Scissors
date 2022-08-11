1a47b2befad84809971da6c9613d37bb3a659096
Mattrix01 <emjay.knight@gmail.com>
2022-08-11 15:28:58 +0100

    result function comparing results created

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//defining variables
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
+let result;
 
 // ever click change inner HTML and run the computer choice function
 possibleChoices.forEach((possibleChoice) =>
@@ -11,6 +13,7 @@ possibleChoices.forEach((possibleChoice) =>
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 // function for computers choice at random out of the possbile choices
@@ -26,4 +29,23 @@ function generateComputerChoice() {
   }
   // adds html of computers choice from its random generated choice
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+// function to output the winner into result by comparing what choices equal to
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw!";
+  } else if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  } else if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lost!";
+  } else if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!";
+  } else if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose!";
+  } else if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  } else if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose!";
+  }
 }
