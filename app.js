96dd90aabe6fb6c4a7fc1ac889a7a8f2942c70e5
Mattrix01 <emjay.knight@gmail.com>
2022-08-11 16:01:10 +0100

    Lizard and Spock added and reconfig if states

--- app.js
+++ app.js
@@ -21,11 +21,15 @@ function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1; // 3 choices, +1 is so starts at 1 not 0
   // if statements to define which each choice equals from random number generated
   if (randomNumber === 1) {
-    computerChoice = "rock";
+    computerChoice = "Scissors";
   } else if (randomNumber === 2) {
-    computerChoice = "paper";
+    computerChoice = "Paper";
   } else if (randomNumber === 3) {
-    computerChoice = "scissors";
+    computerChoice = "Rock";
+  } else if (randomNumber === 4) {
+    computerChoice = "Lizard";
+  } else if (randomNumber === 5) {
+    computerChoice = "Spock";
   }
   // adds html of computers choice from its random generated choice
   computerChoiceDisplay.innerHTML = computerChoice;
@@ -35,18 +39,48 @@ function generateComputerChoice() {
 function getResult() {
   if (computerChoice === userChoice) {
     result = "its a draw!";
-  } else if (computerChoice === "rock" && userChoice === "paper") {
-    result = "you win!";
-  } else if (computerChoice === "rock" && userChoice === "scissors") {
-    result = "you lost!";
-  } else if (computerChoice === "paper" && userChoice === "scissors") {
-    result = "you win!";
-  } else if (computerChoice === "paper" && userChoice === "rock") {
-    result = "you lose!";
-  } else if (computerChoice === "scissors" && userChoice === "rock") {
-    result = "you win!";
-  } else if (computerChoice === "scissors" && userChoice === "paper") {
-    result = "you lose!";
+    // computer wins
+  } else if (computerChoice === "Scissors" && userChoice === "Paper") {
+    result = "Scissors cuts Paper, you lose!";
+  } else if (computerChoice === "Paper" && userChoice === "Rock") {
+    result = "Paper cover Rock, you lose!";
+  } else if (computerChoice === "Rock" && userChoice === "Lizard") {
+    result = "Rock crushes Lizard, you lose!";
+  } else if (computerChoice === "Lizard" && userChoice === "Spock") {
+    result = "Lizard Poisons Spock, you lose!";
+  } else if (computerChoice === "Spock" && userChoice === "Scissors") {
+    result = "Spock smashes Scissors, you lose!";
+  } else if (computerChoice === "Scissors" && userChoice === "Lizard") {
+    result = "Scissors decapitates Lizard, you lose!";
+  } else if (computerChoice === "Lizard" && userChoice === "Paper") {
+    result = "Lizard eats Paper, you lose!";
+  } else if (computerChoice === "Paper" && userChoice === "Spock") {
+    result = "Paper disproves Spock, you lose!";
+  } else if (computerChoice === "Spock" && userChoice === "Rock") {
+    result = "Spock vaporises Rock, you lose!";
+  } else if (computerChoice === "Rock" && userChoice === "scissors") {
+    result = "Rock crushes Scissors, you lose!";
+  } // players wins
+  else if (computerChoice === "Paper" && userChoice === "Scissors") {
+    result = "Scissors cuts Paper, you win!";
+  } else if (computerChoice === "Rock" && userChoice === "Paper") {
+    result = "Paper cover Rock, you win!";
+  } else if (computerChoice === "Lizard" && userChoice === "Rock") {
+    result = "Rock crushes Lizard, you win!";
+  } else if (computerChoice === "Spock" && userChoice === "Lizard") {
+    result = "Lizard Poisons Spock, you win!";
+  } else if (computerChoice === "Scissors" && userChoice === "Spock") {
+    result = "Spock smashes Scissors, you win!";
+  } else if (computerChoice === "Lizard" && userChoice === "Scissors") {
+    result = "Scissors decapitates Lizard, you win!";
+  } else if (computerChoice === "Paper" && userChoice === "Lizard") {
+    result = "Lizard eats Paper, you win!";
+  } else if (computerChoice === "Spock" && userChoice === "Paper") {
+    result = "Paper disproves Spock, you win!";
+  } else if (computerChoice === "Rock" && userChoice === "Spock") {
+    result = "Spock vaporises Rock, you win!";
+  } else if (computerChoice === "Scissors" && userChoice === "Rock") {
+    result = "Rock crushes Scissors, you win!";
   }
   resultDisplay.innerHTML = result;
 }
